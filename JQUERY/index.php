5d1ef780fc1f0350ca6b6cee85d9e14893855dac
mirajane2 <126334852+mirajane2@users.noreply.github.com>
2023-06-10 15:06:54 +0200

    Add files via upload

---
+++ JQUERY/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset = "utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name=viewport contents="width=device-width, initial-scale=1">
+
+        <title>JQuery Introduction</title>
+
+        <link href="css/bootstap.css" rel="stylesheet">
+        <styles>
+            body{
+                padding-top = 50px;
+            }
+        </styles>
+    </head>
+    <body>
+        <nav class="navbar naavbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-hearder">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-targets="#navbar" >
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Introduction</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <!--
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#>Home</a></li>
+                            <li><a href="#about>About</a></li>
+                            <li><a href="#contact>Contact</a></li>
+                        </ul>
+                    -->
+                </div>
+            </div>
+        </nav>
+        <div class="conainer">
+        </div>
+        <script src="js/jquery.js"></script>
+        <script src="js/script.js"></script>
+    </body>
+</html>
+
+
+
+
+
